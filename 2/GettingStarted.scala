c41a7bba894408463720708ca3d80abb8c8af7e8
gifmr <ntkbusy0213@gmail.com>
2019-09-15 19:26:03 +0900

    add

---
+++ 2/GettingStarted.scala
@@ -0,0 +1,13 @@
+object MyModule{
+  
+  def abs(n: Int): Int =
+    if(n < 0) -n
+    else n
+  
+  private def formatAbs(x: Int) = {
+    msg.format(x, abs(x))
+  }
+  
+  def main(args: Array[String]): Unit =
+    println(formatAbs(-42))
+}
