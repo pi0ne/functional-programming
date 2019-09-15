4b38aa357add66bd1988074ce2d381d0874cc00f
gifmr <ntkbusy0213@gmail.com>
2019-09-15 19:46:59 +0900

    update doc.

--- 2/GettingStarted.scala
+++ 2/GettingStarted.scala
@@ -1,10 +1,14 @@
 object MyModule{
   
+  // 純粋関数
   def abs(n: Int): Int =
     if(n < 0) -n
     else n
   
+  // 純粋関数
+  // 複数ステートメントのため中括弧が必要. ステートメントは改行もしくはセミコロンで区切られる
   private def formatAbs(x: Int) = {
+    val msg = "The absolute value of %d is %d"
     msg.format(x, abs(x))
   }
   
